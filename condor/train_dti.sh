d382e81ef14ea0736741de4c14b8fc47e6cb034a
Michael Backenköhler <m.backenkoehler@gmail.com>
2024-12-14 14:12:22 +0100

    condor infrastructure

---
+++ condor/train_dti.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+set -x
+
+cd $HOME/hodge-dti
+
+if [ ! -d venv ]; then
+    python -m venv venv
+fi
+
+source venv/bin/activate
+
+pip install -r requirements.txt
+
+python scripts/train_dti.py
