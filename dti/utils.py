302a283e04331215cde652d567017d6e2156ea0f
Michael Backenköhler <m.backenkoehler@gmail.com>
2024-12-15 15:37:23 +0100

    logging to file and stderr

--- dti/utils.py
+++ dti/utils.py
@@ -15,10 +15,18 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 def init_logging(run_name: Union[str, None] = str(time.time())):
     (OUTPUT / run_name).mkdir(exist_ok=True, parents=True)
     log_file = OUTPUT / run_name / "output.log"
-    logging.basicConfig(
-        filename=log_file,
-        filemode="w",
-        format="%(asctime)s %(name)s %(levelname)s %(message)s",
-        datefmt="%H:%M:%S",
-        level=logging.DEBUG,
-    )
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    console_handler = logging.StreamHandler()
+    file_handler = logging.FileHandler(log_file)
+
+    console_handler.setLevel(logging.INFO)
+    file_handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s", datefmt="%H:%M:%S")
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
