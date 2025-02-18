9a128a39b5c46983b6cfdc3c52d7d89c51b88e1a
Michael Backenköhler <m.backenkoehler@gmail.com>
2025-02-18 14:35:47 +0100

    all pair dti

--- dti/model.py
+++ dti/model.py
@@ -70,7 +70,7 @@ class CombinedModel(nn.Module):
 
         self.ligand_input_size = ligand_input_size
         self.cosine_agg = cosine_agg
-
+        self.embedding_size = embedding_size
         self.protein_mlp = nn.Sequential(
             nn.Linear(protein_input_size, hidden_layer_size),
             nn.SiLU(),
@@ -95,12 +95,11 @@ class CombinedModel(nn.Module):
             nn.Linear(hidden_layer_size, embedding_size),
         )
 
-        joint_embedding_size = embedding_size * (1 if cosine_agg else 2)
         scaled_hidden_dim = hidden_layer_size // 2
         self.combined_mlp = nn.Sequential(
             nn.Dropout(0.05),
-            nn.BatchNorm1d(joint_embedding_size),
-            nn.Linear(joint_embedding_size, hidden_layer_size),
+            nn.BatchNorm1d(embedding_size),
+            nn.Linear(embedding_size, hidden_layer_size),
             nn.SiLU(),
             nn.Linear(hidden_layer_size, scaled_hidden_dim),
             nn.SiLU(),
@@ -139,21 +138,20 @@ class PairCombinedModel(CombinedModel):
         )
 
     def forward(self, protein, ligand):
-        assert ligand.shape[1] == self.ligand_input_size * 2, ligand.shape
-
         protein_emb = self.protein_mlp(protein)
-
-        ligand_a = ligand[:, : self.ligand_input_size]
-        ligand_b = ligand[:, self.ligand_input_size :]
-        ligand_emb_a = self.ligand_mlp(ligand_a)
-        ligand_emb_b = self.ligand_mlp(ligand_b)
-
-        if self.cosine_agg:
-            combined_emb_a = protein_emb * ligand_emb_a
-            combined_emb_b = protein_emb * ligand_emb_b
+        x = self.ligand_mlp(ligand)
+        if x.dim() == 2:  # all pairs
+            x = protein_emb * x
+            n, d = ligand.size(0), self.embedding_size
+            diff_a = (x.view(n, 1, d) - x.view(1, n, d)).reshape(-1, d)  # (b, b, d)
+            diff_b = (x.view(1, n, d) - x.view(n, 1, d)).reshape(-1, d)  # (b, b, d)
+        elif x.dim() == 3:  # pre-defined pairs (k, 2, d)
+            x = x * protein_emb.unsqueeze(1)
+            xa = protein_emb * x[:, 0, :]
+            xb = protein_emb * x[:, 1, :]
+            diff_a = xa - xb
+            diff_b = xb - xa
         else:
-            combined_emb_a = torch.cat([protein_emb, ligand_emb_a], dim=1)
-            combined_emb_b = torch.cat([protein_emb, ligand_emb_b], dim=1)
-        pred_ab = self.combined_mlp(combined_emb_a - combined_emb_b)
-        pred_ba = self.combined_mlp(combined_emb_b - combined_emb_a)
-        return pred_ab - pred_ba
+            assert False
+
+        return self.combined_mlp(diff_a) - self.combined_mlp(diff_b)
--- dti/training.py
+++ dti/training.py
@@ -161,7 +161,6 @@ def train_and_evaluate_model(
             num_epochs=500,
             patience_termination=100,
             patience_lr=20,
-            cosine_agg=False,
             rank_corr_fn=None,
             training_loss=nn.MSELoss(),
         )
@@ -172,7 +171,6 @@ def train_and_evaluate_model(
         ligand_input_size=opts["ligand_dim"],
         embedding_size=opts["embedding_size"],
         protein_input_size=opts["protein_dim"],
-        cosine_agg=opts["cosine_agg"],
     ).to(device)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
