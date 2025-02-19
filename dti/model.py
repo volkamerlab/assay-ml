b64605c8cc9b3ee05bed38cd9000adfd8e6f7b51
Michael Backenköhler <m.backenkoehler@gmail.com>
2025-02-19 13:48:35 +0100

    minor model bug

--- dti/model.py
+++ dti/model.py
@@ -49,10 +49,10 @@ class PairMolecularModel(MolecularModel):
         x = self.embed(molecule)
         if x.dim() == 2:  # full (b, b) pairs
             n, d = molecule.size(0), self.embedding_size
-            diff_a = (x.view(n, 1, d) - x.view(1, n, d)).reshape(-1, d)  # (b, b, d)
-            diff_b = (x.view(1, n, d) - x.view(n, 1, d)).reshape(-1, d)  # (b, b, d)
+            diff_a = (x.view(n, 1, d) - x.view(1, n, d)).reshape(n**2, d)  # (b^2, d)
+            diff_b = (x.view(1, n, d) - x.view(n, 1, d)).reshape(n**2, d)  # (b^2, d)
         elif x.dim() == 3:  # assume pre-defined pairs (k, 2, d)
-            diff_a = x[:, 0, :] - x[:, 1, :]  # (k, 2, d)
+            diff_a = x[:, 0, :] - x[:, 1, :]  # (k, d)
             diff_b = x[:, 1, :] - x[:, 0, :]
         return self.readout(diff_a) - self.readout(diff_b)
 
@@ -144,45 +144,15 @@ class PairCombinedModel(CombinedModel):
         if x.dim() == 2:  # all pairs
             x = protein_emb * x
             n, d = ligand.size(0), self.embedding_size
-            diff_a = (x.view(n, 1, d) - x.view(1, n, d)).reshape(-1, d)  # (b, b, d)
-            diff_b = (x.view(1, n, d) - x.view(n, 1, d)).reshape(-1, d)  # (b, b, d)
+            diff_a = (x.view(n, 1, d) - x.view(1, n, d)).reshape(n**2, d)
+            diff_b = (x.view(1, n, d) - x.view(n, 1, d)).reshape(n**2, d)
         elif x.dim() == 3:  # pre-defined pairs (k, 2, d)
             x = x * protein_emb.unsqueeze(1)
-            xa = protein_emb * x[:, 0, :]
-            xb = protein_emb * x[:, 1, :]
+            xa = x[:, 0, :]
+            xb = x[:, 1, :]
             diff_a = xa - xb
             diff_b = xb - xa
         else:
             assert False
 
         return self.combined_mlp(diff_a) - self.combined_mlp(diff_b)
-
-    # def forward(self, protein, ligand):
-    #     protein_emb = self.protein_mlp(protein)
-    #
-    #     if ligand.shape[1] == self.ligand_input_size * 2:
-    #         ligand_a = ligand[:, : self.ligand_input_size]
-    #         ligand_b = ligand[:, self.ligand_input_size :]
-    #         ligand_emb_a = self.ligand_mlp(ligand_a)
-    #         ligand_emb_b = self.ligand_mlp(ligand_b)
-    #
-    #         if self.cosine_agg:
-    #             combined_emb_a = protein_emb * ligand_emb_a
-    #             combined_emb_b = protein_emb * ligand_emb_b
-    #         else:
-    #             combined_emb_a = torch.cat([protein_emb, ligand_emb_a], dim=1)
-    #             combined_emb_b = torch.cat([protein_emb, ligand_emb_b], dim=1)
-    #         pred_ab = self.combined_mlp(combined_emb_a - combined_emb_b)
-    #         pred_ba = self.combined_mlp(combined_emb_b - combined_emb_a)
-    #         return pred_ab - pred_ba
-    #     elif ligand.shape[1] == self.ligand_input_size:
-    #         ligand_emb = self.ligand_mlp(ligand)
-    #
-    #         if self.cosine_agg:
-    #             combined_emb = protein_emb * ligand_emb
-    #         else:
-    #             combined_emb = torch.cat([protein_emb, ligand_emb], dim=1)
-    #         output = self.combined_mlp(combined_emb)
-    #         return output
-    #     else:
-    #         raise ValueError(f"Unexpected ligand shape {ligand.shape}")
