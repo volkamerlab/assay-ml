b7633c92dad6b8cd704d486c80e6d8cbb494d010
Michael Backenköhler <m.backenkoehler@gmail.com>
2025-02-19 14:52:59 +0100

    pretty print status

--- status.sh
+++ status.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 output_dir=~/hodge-dti/data/output/
 output_files=$(find $output_dir -name output.log | sort)
+
+printf "%-40s %-10s %-10s %-15s %-10s\n" "Dataset" "Epoch" "Fold" "Test Rank Corr" "Finished"
+echo "--------------------------------------------------------------------------------------------------"
+
 for f in $output_files; do
-    echo $f
-    for i in 0 1 2 3 4; do
-        cur_performance=$(grep "Test Rank Corr:" $f | grep "Fold: $i" | tail -n 1)
-        cur_corr=$(echo $cur_performance | awk '{print $NF}')
-        cur_epoch=$(echo $cur_performance | awk '{print $5}' | cut -c -3)
-        echo "fold=$i epoch=$cur_epoch: $cur_corr"
-    done
+    line=$(grep "Test Rank Corr:" $f | tail -n 1)
+    dataset=$(echo "$line" | awk '{print $3}')
+    epoch=$(echo "$line" | awk '{print $5}')
+    fold=$(echo "$line" | awk '{print $7}')
+    rank_corr=$(echo "$line" | awk '{print $11}')
+    finished=$(grep -q "finished" $f && echo "x" || echo "")
+    printf "%-40s %-10s %-10s %-15s %-10s\n" "$dataset" "$epoch" "$fold" "$rank_corr" "$finished"
 done
