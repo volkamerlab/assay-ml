70b7bd55885d739b063af322a1e35355f5201509
Michael Backenköhler <m.backenkoehler@gmail.com>
2025-02-21 10:18:27 +0100

    better status script

--- status.sh
+++ status.sh
@@ -2,15 +2,24 @@
 output_dir=~/hodge-dti/data/output/
 output_files=$(find $output_dir -name output.log | sort)
 
-printf "%-32s %-6s %-5s %-10s %-10s\n" "Dataset" "Epoch" "Fold" "Test Corr" "State"
-echo "-----------------------------------------------------------------"
+printf "%-32s %-6s %-5s %-10s %-10s\n" "Run    " "Epoch" "Fold" "Test Corr" "State"
 
 upcoming=""
+old_fold="-1"
 for f in $output_files; do
     line=$(rg "Test Rank Corr:" $f | tail -n 1)
     dataset=$(echo "$line" | awk '{print $3}')
     epoch=$(echo "$line" | awk '{print $5}')
     fold=$(echo "$line" | awk '{print $7}')
+    if [ "$old_fold" != "$fold" ]; then
+        if [ "$old_fold" == "-1" ]; then
+            echo "================================================================="
+            old_fold=$fold
+        elif [ "$fold" != "" ]; then
+            echo "-----------------------------------------------------------------"
+            old_fold=$fold
+        fi
+    fi
     rank_corr=$(echo "$line" | awk '{print $11}')
     last_line=$(tail -n 1 "$f")
     if echo "$last_line" | rg -q "Error"; then
@@ -20,15 +29,23 @@ for f in $output_files; do
     else
         state="Running"
     fi
-    if [ "$dataset" == "" ]; then
+    if [ "$dataset" == "" ] && [ "$state" != "Error" ]; then
         upcoming="$upcoming $f"
     else
-        printf "%-32s %-6s %-5s %-10s %-10s\n" "$dataset" "$epoch" "$fold" "$rank_corr" "$state"
+        if [ "$dataset" == "" ]; then
+            fname=$(echo "$f" | cut -d '/' -f 7)
+            fname=$(echo "[$fname]")
+            printf "%-56s %-10s\n" "$fname" "$state"
+        else
+            printf "%-32s %-6s %-5s %-10s %-10s\n" "$dataset" "$epoch" "$fold" "$rank_corr" "$state"
+        fi
     fi
 done
 
-printf "\nOther:\n------\n"
-for f in $upcoming; do
-    echo $f
-    echo " $(tail -n 1 $f)"
-done
+if [ "$upcoming" != "" ]; then
+    printf "\nOther:\n------\n"
+    for f in $upcoming; do
+        echo $f
+        echo " $(tail -n 1 $f)"
+    done
+fi
