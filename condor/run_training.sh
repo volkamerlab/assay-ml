1bc91d3cfb07149a49c0d6acddaa2bdbdafc216b
Michael Backenköhler <m.backenkoehler@gmail.com>
2025-09-12 17:13:30 +0200

    switch to uv

--- condor/run_training.sh
+++ condor/run_training.sh
@@ -5,14 +5,10 @@ set -x
 
 cd $HOME/hodge-dti
 
-if [ ! -d venv ]; then
-    python -m venv venv
-fi
-
-source venv/bin/activate
-
-#pip install -r requirements.txt
-#pip install -e .
+#uv sync
+#uv pip install -e .
 
 export CUBLAS_WORKSPACE_CONFIG=:4096:8
-python $1 $2 $3 $4 $5
+#export JOBLIB_TEMP_FOLDER=/home/michael.backenkoehler/hodge-dti/data/tmp
+echo "python $1 $2 $3 $4 $5 $6"
+uv run $1 $2 $3 $4 $5 $6
