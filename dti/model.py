ed9c1e395265157deefacc9c9ccff4f70ea796d4
joschka-gross <jgross@cs.uni-saarland.de>
2025-09-15 10:41:28 +0200

    fix x_dist masking logic

--- dti/model.py
+++ dti/model.py
@@ -281,8 +281,9 @@ class MoleculeBayesianSetRankModel(MoleculeSetRank):
         attn_mask = make_block_diag_mask(set_ids, num_heads=self.num_heads)
         attn_mask = torch.logical_or(make_asymmetric_mask(sample_mask))
         x_ligand = self.embed_ligand(ligand)
-        x_dist = self.distribution_encoder(dist)
-        x_dist[~mask] = self.default_dist_emb
+        x_dist = (1 - sample_mask.float()) * self.distribution_encoder(
+            dist
+        ) + sample_mask.float() * self.default_dist_emb
         x = self.combine_repr(torch.cat((x_ligand, x_dist), 1))
         h = self.set_transformer(x, attn_mask=attn_mask)
         return self.ouput(h).squeeze()
@@ -387,8 +388,9 @@ class ComplexBayesianSetRankModel(MoleculeBayesianSetRankModel):
         attn_mask = torch.logical_or(attn_mask, make_asymmetric_mask(sample_mask))
         x_ligand = self.embed_ligand(ligand)
         x_protein = self.embed_protein(protein)
-        x_dist = self.distribution_encoder(dist)
-        x_dist[sample_mask] = self.default_dist_emb
+        x_dist = (1 - sample_mask.float()) * self.distribution_encoder(
+            dist
+        ) + sample_mask.float() * self.default_dist_emb
         x = self.combine_repr(torch.cat((x_ligand, x_protein, x_dist), 1))
         h = self.set_transformer(x, attn_mask=attn_mask)
         return self.ouput(h).squeeze()
