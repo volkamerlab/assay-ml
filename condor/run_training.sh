de759738b73eab38c8bc31dd981af497b904c3a2
Michael Backenköhler <m.backenkoehler@gmail.com>
2025-09-15 08:42:39 +0200

    adjustment to new train scipt

--- condor/run_training.sh
+++ condor/run_training.sh
@@ -11,4 +11,4 @@ cd $HOME/hodge-dti
 export CUBLAS_WORKSPACE_CONFIG=:4096:8
 #export JOBLIB_TEMP_FOLDER=/home/michael.backenkoehler/hodge-dti/data/tmp
 echo "python $1 $2 $3 $4 $5 $6"
-uv run $1 $2 $3 $4 $5 $6
+uv run $1 --seed $2 --dataset $3 --method $4 --fold $5 --mol-feat $6
