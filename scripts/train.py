a7402d1c859c3f6dd32f5bfba2527cd90661f31d
Michael Backenköhler <m.backenkoehler@gmail.com>
2025-09-16 10:40:48 +0200

    pfn: less sets per batch

--- scripts/train.py
+++ scripts/train.py
@@ -94,7 +94,11 @@ def setup(method: Method, dataset: str) -> Tuple[type, type, type, Callable]:
 
 
 def model_and_dataset(method: Method, mol_only: bool) -> Tuple[type, type, type]:
-    msa = partial(MultiSetActivityDataset, max_set_size=1000)
+    msa = partial(
+        MultiSetActivityDataset,
+        max_set_size=1000,
+        sets_per_batch=(10 if method == Method.PFN else 20),
+    )
     shuffled_multiset = partial(msa, inter_assay=True)
     match method:
         case Method.PFN if mol_only:
