b4f37f1b144c5d98a743a7d38ee82d7ca60c1bc0
Michael Backenköhler <m.backenkoehler@gmail.com>
2025-09-17 09:44:55 +0200

    minor fix (last ci)

--- scripts/train.py
+++ scripts/train.py
@@ -308,11 +308,11 @@ def run_synthetic(
             mol_featurizer=MolFingerprint("morgan"),
             target=ACT,
             info_cols=[],
+            max_set_size=1000,
         ),
         batch_size=1,
         shuffle=False,
         num_workers=0,
-        max_set_size=1000,
     )
 
     training_loss = loss_fn(method, nn.SmoothL1Loss(reduction="none"))
