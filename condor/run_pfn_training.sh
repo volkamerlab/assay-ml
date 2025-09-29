9dee46aaf0170414d685e7c5887ede495678cc1d
Michael Backenköhler <m.backenkoehler@gmail.com>
2025-09-29 15:15:48 +0200

    pfn weighted loss training

---
+++ condor/run_pfn_training.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+set -x
+
+cd $HOME/hodge-dti
+
+#uv sync
+#uv pip install -e .
+
+export CUBLAS_WORKSPACE_CONFIG=:4096:8
+#export JOBLIB_TEMP_FOLDER=/home/michael.backenkoehler/hodge-dti/data/tmp
+echo "uv run $1 --seed $2 --dataset $3 --method $4 --fold $5 --mol-feat $6 --unmasked-weight $7"
+uv run $1 --seed $2 --dataset $3 --method $4 --fold $5 --mol-feat $6 --unmasked-weight $7
