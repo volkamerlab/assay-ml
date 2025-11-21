d5a102ee464963faf5353904cec3954fe5b37b9f
Michael Backenköhler <m.backenkoehler@gmail.com>
2025-11-21 10:11:03 +0100

    validation run script for hpo

---
+++ condor/run_pfn_training_val.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+set -x
+
+cd $HOME/assay-ml
+
+
+export CUBLAS_WORKSPACE_CONFIG=:4096:8
+export CACHE_DIR=/scratch/chair_volkamer/michael.backenkoehler/cache
+export GRAPH_CACHE_DIR=/tmp/graph_cache
+mkdir -p "$GRAPH_CACHE_DIR"
+mkdir -p "$CACHE_DIR"
+
+uv run $1 --num-epochs 100 --test False --fold $2 --seed 0 --n-bins $3 --mol-feat $4 --unmasked-weight $5 --property-set-ratio $6 --norm $7 --dropout $8 --lr $9 --obj ${10} --act ${11}
