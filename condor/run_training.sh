c921dbcde4f615ee121fd07a275954dfef08135a
Michael Backenköhler <m.backenkoehler@gmail.com>
2026-09-09 13:44:09 +0200

    training setup

--- condor/run_training.sh
+++ condor/run_training.sh
@@ -10,8 +10,8 @@ cd $HOME/hodge-dti
 
 export CUBLAS_WORKSPACE_CONFIG=:4096:8
 #export JOBLIB_TEMP_FOLDER=/home/michael.backenkoehler/hodge-dti/data/tmp
-ASSAY_ML_IDENT="${3}_allsets_${5}" uv run $1 $2 $3 allsets $5 $6 &
-ASSAY_ML_IDENT="${3}_sets_${5}" uv run $1 $2 $3 sets $5 $6 &
-ASSAY_ML_IDENT="${3}_ic50_${5}" uv run $1 $2 $3 ic50 $5 $6 &
+ASSAY_ML_IDENT="${3}_allsets_${5}" uv run $1 $2 $3 allsets $4  &
+ASSAY_ML_IDENT="${3}_sets_${5}" uv run $1 $2 $3 sets $4  &
+ASSAY_ML_IDENT="${3}_ic50_${5}" uv run $1 $2 $3 ic50 $4  &
 
 wait
